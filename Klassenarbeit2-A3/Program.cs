189640ea6769b247457c5fb3d4b63a29001d66ae
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-02-09 07:15:23 +0100

    FTE1-Klassenarbeit2-A3

---
+++ Klassenarbeit2-A3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Klassenarbeit2_A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dauer = 0, i, k = 0;
+            double betrag, zinssatz = 0.0;
+            char tarif;
+            Console.WriteLine("Anlagebetrag? ");
+            betrag = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Tarif? ");
+            tarif = Convert.ToChar(Console.ReadLine());
+            switch (tarif)
+            {
+                case 'A': zinssatz = 0.2; dauer = 3; break;
+                case 'B': zinssatz = 0.5; dauer = 5; break;
+                case 'C': zinssatz = 1.0; dauer = 10; break;
+                case 'D': zinssatz = 2.0; dauer = 15; break;
+
+            }
+            for (i = 1; i <= dauer; i++)
+            {
+                betrag = betrag * (1 + zinssatz / 100);
+                Console.WriteLine($"{i,2}. Jahr: {betrag,9:F2} Euro");
+                if (k == 0 && betrag >= 10000)
+                    k = i;
+            }
+            if (k > 0 && k < dauer)
+                Console.WriteLine($"Kündigung ist möglich nach {k} Jahr(en).");
+            else
+                Console.WriteLine("Kündigung ist nicht möglich.");
+        }
+    }
+}
