1d16d63589ade86f97f1d535ff882dc03e886441
Crazy Koodaa <crazykoodaa@vivaldi.net>
2022-04-09 15:57:14 +0200

    Benzinverbrauch und Zinsberechnung mit Methoden

---
+++ Benzinverbrauch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Benzinverbrauch
+{
+    class KFZ
+    {
+        public static double BerechneBenzinverbrauch(int gefahreneKilometer, double verbrauchteLiter) => verbrauchteLiter / gefahreneKilometer * 100;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Zahlen 1 - 6");
+
+            var verbrauch = Console.ReadKey(true).KeyChar switch
+            {
+                '1' => KFZ.BerechneBenzinverbrauch(96, 4.2),
+                '2' => KFZ.BerechneBenzinverbrauch(178, 10.2),
+                '3' => KFZ.BerechneBenzinverbrauch(63, 7.8),
+                '4' => KFZ.BerechneBenzinverbrauch(158, 20),
+                '5' => KFZ.BerechneBenzinverbrauch(260, 38),
+                '6' => KFZ.BerechneBenzinverbrauch(370, 50),
+                _ => -1,
+            };
+            Console.WriteLine($"{verbrauch:F2}");
+        }
+    }
+}
---
+++ Zinsberechnung mit Methoden/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zinsberechnung_mit_Methoden
+{
+    class Sparkonto
+    {
+        
+        private static double berechneZinsen(double guthaben, double zinssatz) => guthaben * (zinssatz / 100);
+        public static double berechneNeuesGuthaben(double guthaben, double zinssatz) => berechneZinsen(guthaben, zinssatz) + guthaben;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Sparkonto.berechneNeuesGuthaben(1000, 2.5));
+        }
+    }
+}
