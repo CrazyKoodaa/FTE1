2a37e41e5326da8e897b93b3c19e7aa0944994d0
Benjamin Meder <Benjamin@meder.tv>
2022-07-13 11:30:43 +0200

    palindrom

---
+++ Palindrom/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            string temp = "tsest";
+            string palin = new string(temp.Reverse().ToArray());
+            if (temp == palin)
+                Console.WriteLine("Ist Palin");
+            else
+                Console.WriteLine("Nicht Palin");
+        }
+    }
+}
---
+++ Passwort_Name_Datum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Passwort_Name_Datum
+{
+    public static class NeueMethoden
+    {
+        public static string bildePasswort(string name, string datum)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
